8480f72ac208bf3f99a4ad6817364effd0f55053
aliaxy <aruvelut00@163.com>
2024-03-29 13:36:16 +0800

    :sparkles: feat: add wezterm config

--- fish/conf.d/alias.fish
+++ fish/conf.d/alias.fish
@@ -1,2 +1,4 @@
 alias vi nvim
 alias vim nvim
+alias ls lsd
+alias la "ls -a"
---
+++ wezterm/wezterm.lua
@@ -0,0 +1,27 @@
+local wezterm = require("wezterm")
+
+local config = {
+	font_size = 12,
+	font = wezterm.font_with_fallback({
+		"Fira Code",
+		"Source Han Sans CN",
+	}),
+	color_scheme = "Catppuccin Mocha",
+
+	use_fancy_tab_bar = false,
+	hide_tab_bar_if_only_one_tab = true,
+	window_decorations = "RESIZE",
+	show_new_tab_button_in_tab_bar = false,
+
+	default_cursor_style = "BlinkingBar",
+	cursor_blink_rate = 500,
+
+	window_padding = {
+		left = 10,
+		right = 10,
+		top = 10,
+		bottom = 5,
+	},
+}
+
+return config
